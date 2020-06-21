9c67d5e27bb4c03da1f83c23e45e4b0d7b0f178b
MaoKo <MaoKo@riseup.net>
2020-06-21 19:09:15 +0200

    basic macro

---
+++ a.asm
@@ -0,0 +1,13 @@
+
+include "asmcmd.inc"
+include "xcalm.alm"
+include "ensure.alm"
+include "identifier.inc"
+include "dalvik.alm"
+include "dex.alm"
+
+_start:
+	move/from16 v0, v65000
+	return v0
+
+
