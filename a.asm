ff96591b7fe013ce80a57856121349e83754562f
MaoKo <Alexandre.Audibert@outlook.fr>
2020-07-01 20:36:18 +0200

    adding sha1 (in work)

--- a.asm
+++ a.asm
@@ -1,5 +1,12 @@
 
 include "gdalvik.inc"
 
-_ ror $02, 110B, $01
-display _+"0", $0A
+virtual at $00
+_test::
+    ;db "The quick brown fox jumps over the lazy cog"
+    db "A"
+    _test._len = $
+end virtual
+
+_ sha1 _test
+dd _
